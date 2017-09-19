533973f6df559349ea36663adc4405f9588025d6
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-09-19 15:08:56 -0400

    dunzo - also it was quite funzo

---
+++ calculator.rb
@@ -0,0 +1,17 @@
+def sum(num_array)
+  total_num = 0
+  num_array.each do |num|
+    total_num += num
+  end
+  return total_num
+end
+
+
+
+def add(num1, num2)
+  num1 + num2
+end
+
+def subtract(num1, num2)
+  num1 - num2
+end
